9aad24a9f1bebf402d1c81322c47b5be6a11d3d8
nathancusick-arch <nathancusick@servelegal.co.uk>
2025-11-24 10:49:05 +0000

    keepalive (updated Tue Nov 25 06:56:55 UTC 2025)

--- Deliveroo Raw Data Mapper.py
+++ Deliveroo Raw Data Mapper.py
@@ -8,7 +8,7 @@ st.write("""
           1. Export the previous 2 weeks worth of data
           2. Drop the file in the below box, it should then give you the output file in your downloads
           3. Standard bits - Check data vs previous week, remove data already reported
-          5. Done.
+          4. Done.
           """)
 
 # ============================================================
